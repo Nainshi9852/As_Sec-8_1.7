fd8e82907e9c1ffc26211a429433e60783182821
nainshisingh985@gmail.com <nainshisingh985@gmail.com>
2023-07-27 19:32:21 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SinglyLinkedList.Sl;
+
+namespace SinglyLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+            Console.ReadKey();
+        }
+
+        public static void runApp()
+        {
+            SingleLinkedList list = new SingleLinkedList();
+            Node n = new Node();
+            n.Data = "root";
+            list.add(n);
+            for (int i = 0; i < 10; i++)
+            {
+                Node node = new Node();
+                node.Data = Convert.ToString(i);
+                list.add(node);
+            }
+
+            Console.WriteLine("length of singly linked list=" + list.getLengthOfList());
+
+            n = list.getRoot();
+            while (n != null)
+            {
+                Console.WriteLine(n.Data);
+                n = list.getNext();
+            }
+        }
+
+    }
+}
